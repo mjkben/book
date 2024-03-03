4eb1f2bec67da1218a73e31999f0471a2b7493ef
MjkB <mjknews@gmail.com>
2024-03-03 20:46:20 +0100

    first commit

---
+++ book/gitpush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Documents/MES-COURS/JUPYTER/ 
+jupyter-book build book/
+cp -r ~/Documents/MES-COURS/JUPYTER/* ~/Git-Local_Repo/book-github/
+
+cd ~/Git-Local_Repo/book-github/
+git add ./*
+git commit -m "first commit"
+# git remote add origin git@github.com:<user>/<repository-name>.git
+git push -u origin main
